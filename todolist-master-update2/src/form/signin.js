1b98b03ab5c09f5977e136fdedc5fd11761719d7
NhamHoangAnh <47606391+NhamHoangAnh@users.noreply.github.com>
2019-02-14 17:43:50 +0700

    Add files via upload

--- todolist-master-update2/src/form/signin.js
+++ todolist-master-update2/src/form/signin.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react'
 import { Button, Col, Row, FormGroup, Label, Input, TabPane } from 'reactstrap'
-import {withRouter} from 'react-router-dom'
- class signin extends Component {
-    constructor(props){
+import { withRouter } from 'react-router-dom'
+class signin extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            signined: false
+        this.state = {
+            signined: false,
+            email:"",
+            password:"",
+            data:[],
+            text:""
         }
     }
-    onClick=()=>{
-    this.props.Signin()
-      let path = "/";
-    this.props.history.push(path)
+    onClick = () => {
+        const {text} = this.state;
+        this.fetchSignin(text)
     }
+    fetchSignin = () => {
+        fetch("", {
+            method: "Post",
+            headers: {
+                //Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: this.state.email,          // hardcode mai lap cai lay email va password
+                password: this.state.password,
+            })
+        })
+            .then(res => {
+                if (res.authentic) { //authentic ?
+                    this.props.Signin()
+                    let path = "/";
+                    this.props.history.push(path)
+                } else {
+                    throw Error("Please check your email or password")
+                }
+            })
+            .then(json =>{
+                this.setState({
+                    signined: true,
+                    data: json
+                })
+            })
+    }
+    handleEmail =(e) =>{
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlePassword =(e) =>{
+        this.setState({
+            password: e.target.value
+        })
+    }
+    
     render() {
         return (
             <TabPane tabId="1">
@@ -21,7 +63,7 @@ import {withRouter} from 'react-router-dom'
                         <FormGroup>
                             <Label for="email" >Email</Label>
                             <br />
-                            <Input type="email" id="email" name="email" placeholder="email123@gmail.com"></Input>
+                            <Input type="email" id="email" name="email" placeholder="email123@gmail.com" onChange={this.handleEmail} value={this.state.value}></Input>
                         </FormGroup>
                     </Col>
                 </Row>
@@ -30,12 +72,12 @@ import {withRouter} from 'react-router-dom'
                         <FormGroup>
                             <Label for="password">Password</Label>
                             <br />
-                            <Input type="password" id="password" name="password" placeholder="Enter your password..."></Input>
+                            <Input type="password" id="password" name="password" placeholder="Enter your password..." onChange={this.handlePassword} value={this.state.value}></Input>
                         </FormGroup>
                     </Col>
                 </Row>
-                    <Button onClick={this.onClick} style={{ marginTop: "10px" }} color="primary">Sign in</Button>
-                    
+                <Button onClick={this.onClick} style={{ marginTop: "10px" }} color="primary">Sign in</Button>
+
             </TabPane>
         )
     }
--- todolist-master-update2/src/form/signup.js
+++ todolist-master-update2/src/form/signup.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react'
 import { Col, Row, FormGroup, Label, Input, Button, TabPane } from 'reactstrap'
-import {Link, withRouter} from 'react-router-dom';
- class signup extends Component {
-    onClick=()=>{
-     let path = "/"; 
-     console.log(path)
-      this.props.history.push(path)
-      }
+import { Link, withRouter } from 'react-router-dom';
+class signup extends Component {
+
+    constructor(){
+        super();
+        this.state ={
+            isRegistering: false,
+            name:"",
+            email:"",
+            password:"",
+            passwordRewrite:"",
+            data:[],
+            text:""
+        }
+    }
+    onClick = () => {
+        let passwordCf = this.state.password
+        let passwordcheck = this.state.passwordRewrite
+        const {text} = this.state;
+        if (passwordCf === passwordcheck){
+           this.fetchRegisration(text)
+           alert("Congratulation! Your registration is success!")
+        } else {
+            alert("Your password is not correct")
+        }
+        // let path = "/";
+        // console.log(path)
+        // this.props.history.push(path)
+    }
+    fetchRegisration =() =>{
+        this.setState({
+            isRegistering: true,
+        })
+        fetch("",{
+            method: "POST",
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+            .then(res =>res.json())
+            .then(res =>{
+                this.setState({
+                    data: res,
+                    isRegistering: false,
+                })
+            })
+    }
+    handleName =(e) =>{
+        this.setState({
+            name: e.target.value
+        })
+    }
+    handleEmail =(e) =>{
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlePassword =(e) =>{
+        this.setState({
+            password: e.target.value
+        })
+    }
+    handlePasswordRewrite =(e) =>{
+        this.setState({
+            passwordRewrite: e.target.value
+        })
+    }
     render() {
         return (
             <TabPane tabId="2">
@@ -15,7 +77,7 @@ import {Link, withRouter} from 'react-router-dom';
                         <FormGroup>
                             <Label for="input" >Name</Label>
                             <br />
-                            <Input type="text" id="input" name="input" placeholder="ABC XYZ"></Input>
+                            <Input type="text" id="input" name="input" placeholder="ABC XYZ" onChange={this.handleName} value={this.state.name}></Input>
                         </FormGroup>
                     </Col>
                 </Row>
@@ -24,7 +86,7 @@ import {Link, withRouter} from 'react-router-dom';
                         <FormGroup>
                             <Label for="email" >Email</Label>
                             <br />
-                            <Input type="email" id="email" name="email" placeholder="email123@gmail.com"></Input>
+                            <Input type="email" id="email" name="email" placeholder="email123@gmail.com" onChange={this.handleEmail} value={this.state.email}></Input>
                         </FormGroup>
                     </Col>
                 </Row>
@@ -33,7 +95,7 @@ import {Link, withRouter} from 'react-router-dom';
                         <FormGroup>
                             <Label for="password" >Password</Label>
                             <br />
-                            <Input type="password" id="password" name="password" placeholder="Enter your password..."></Input>
+                            <Input type="password" id="password" name="password" placeholder="Enter your password..." onChange={this.handlePassword} value={this.state.password}></Input>
                         </FormGroup>
                     </Col>
                 </Row>
@@ -42,7 +104,7 @@ import {Link, withRouter} from 'react-router-dom';
                         <FormGroup>
                             <Label for="password" >Rewrite password</Label>
                             <br />
-                            <Input type="password" id="password" name="password" placeholder="Enter your password..."></Input>
+                            <Input type="password" id="password" name="password" placeholder="Enter your password..." onChange={this.handlePasswordRewrite} value={this.state.passwordRewrite}></Input>
                         </FormGroup>
                     </Col>
                 </Row>
